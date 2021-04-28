abffa59d1ced4a2ab3fcd0ee7b090b5651f3db2d
Yuan Gong <gongyuan94@gmail.com>
2021-04-28 13:34:37 +0000

    update env

--- kubeflow/env.sh
+++ kubeflow/env.sh
@@ -1,10 +1,8 @@
-export KF_NAME=<kubeflow-cluster-name>
-export KF_PROJECT=<gcp-project-id>
-export KF_DIR=<kubeflow-download-path>
-export MGMT_NAME=<management-cluster-name>
+export KF_NAME=dev-4-28
+export KF_PROJECT=gongyuan-dev
+export KF_DIR="$PWD"
+export MGMT_NAME="mgmt-3-23"
 export MGMTCTXT=${MGMT_NAME}
-export LOCATION=<zone>
-
+export LOCATION=asia-east1-c
+export ADMIN_EMAIL=gongyuan@google.com
 export ASM_LABEL=asm-192-1
-
-
