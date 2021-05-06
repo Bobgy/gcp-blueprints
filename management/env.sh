7d12dbdfc858f26d78f6e8f8181a09edbeced743
Yuan Gong <gongyuan94@gmail.com>
2021-05-06 08:20:52 +0000

    feat: upgrade management cluster

--- management/env.sh
+++ management/env.sh
@@ -1,4 +1,4 @@
-export MGMT_PROJECT=<the project where you deploy your management cluster>
-export MGMT_DIR=<path to your management cluster configuration directory>
-export MGMT_NAME=<name of your management cluster>
-export LOCATION=<location of your management cluster>
+export MGMT_PROJECT=gongyuan-dev
+export MGMT_DIR=$(pwd)
+export MGMT_NAME=mgmt-3-23
+export LOCATION=asia-east1-c
